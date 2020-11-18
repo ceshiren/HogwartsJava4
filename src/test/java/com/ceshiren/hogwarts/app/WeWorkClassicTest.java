db2859dffcd557ea2762653394cba3f5dbb77a40
seveniruby <seveniruby@gmail.com>
2020-11-18 21:35:40 +0800

    添加appium依赖与基本用例

---
+++ src/test/java/com/ceshiren/hogwarts/app/WeWorkClassicTest.java
@@ -0,0 +1,35 @@
+package com.ceshiren.hogwarts.app;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class WeWorkClassicTest {
+    @Test
+    void search() throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("platformName", "android");
+        capabilities.setCapability("deviceName", "ceshiren.com");
+        capabilities.setCapability("appPackage", "com.tencent.wework");
+        capabilities.setCapability("appActivity", ".launch.LaunchSplashActivity");
+        capabilities.setCapability("noReset", "true");
+        AppiumDriver<MobileElement> driver = new AppiumDriver<>(
+                new URL("http://0.0.0.0:4723/wd/hub"),
+                capabilities);
+
+        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        driver.findElement(MobileBy.id("i6n")).click();
+        driver.findElement(MobileBy.id("i68")).click();
+        driver.findElement(MobileBy.id("gpg")).sendKeys("sihan");
+
+
+    }
+}
