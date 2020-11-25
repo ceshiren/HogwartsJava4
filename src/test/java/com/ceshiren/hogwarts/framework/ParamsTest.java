a77f80dbe8854ef37a75576791cbe1ace8234a9c
seveniruby <seveniruby@gmail.com>
2020-11-25 21:10:14 +0800

    测试数据的数据驱动示例

--- src/test/java/com/ceshiren/hogwarts/framework/ParamsTest.java
+++ src/test/java/com/ceshiren/hogwarts/framework/ParamsTest.java
@@ -1,5 +1,9 @@
 package com.ceshiren.hogwarts.framework;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -7,6 +11,11 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
@@ -29,15 +38,24 @@ public class ParamsTest {
     @ParameterizedTest
 //    @ValueSource(strings = {"search demo1","search demo2"})
     @MethodSource()
-    void search(String keyword){
-        ChromeDriver driver= new ChromeDriver();
+    void search(String keyword) {
+        //todo: 测试步骤的数据驱动
+        ChromeDriver driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("https://ceshiren.com");
         driver.findElement(By.id("search-button")).click();
         driver.findElement(By.id("search-term")).sendKeys(keyword);
-    }
-    static Stream<String> search(){
-        return Stream.of("apple", "banana");
 
+    }
+
+    static List<String> search() throws IOException {
+//        return Stream.of("apple", "banana");
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        TypeReference typeReference = new TypeReference<List<String>>() {
+        };
+        List<String> keywords = mapper.readValue(
+                ParamsTest.class.getResourceAsStream("/framework/search.yaml"),
+                typeReference);
+        return keywords;
     }
 }
