cb35e33b5052ba5504b6acfaa66e5a53bba69a6d
seveniruby <seveniruby@gmail.com>
2020-11-28 14:49:28 +0800

    测试用例裂变与变量替换

--- src/test/java/com/ceshiren/hogwarts/framework/ParamsTest.java
+++ src/test/java/com/ceshiren/hogwarts/framework/ParamsTest.java
@@ -1,23 +1,12 @@
 package com.ceshiren.hogwarts.framework;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
-import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -52,7 +41,7 @@ public class ParamsTest {
 
     }
 
-    static Stream<TestCase> search() throws IOException {
+    static List<TestCase> search() throws IOException {
 //        return Stream.of("apple", "banana");
         ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
 //        TypeReference typeReference = new TypeReference<List<String>>() {
@@ -60,6 +49,7 @@ public class ParamsTest {
         TestCase testCase = mapper.readValue(
                 ParamsTest.class.getResourceAsStream("/framework/search.yaml"),
                 TestCase.class);
-        return Stream.of(testCase);
+        return testCase.testcaseGenerate();
+
     }
 }
--- src/test/java/com/ceshiren/hogwarts/framework/TestCase.java
+++ src/test/java/com/ceshiren/hogwarts/framework/TestCase.java
@@ -8,48 +8,90 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicReference;
 
 public class TestCase {
     public List<String> data;
     public List<HashMap<String, Object>> steps;
     private ChromeDriver driver;
     private WebElement currentElement;
+    public int index = 0;
 
-    public void run(){
-        steps.forEach(step->{
-            if (step.keySet().contains("chrome")){
+
+    /**
+     * 测试用例裂变，基于数据自动生成多份测试用例
+     * @return
+     */
+    public List<TestCase> testcaseGenerate() {
+        List<TestCase> testCaseList=new ArrayList<>();
+        for (int i = 0; i < data.size(); i++) {
+            TestCase testcaseNew = new TestCase();
+            testcaseNew.index=i;
+            testcaseNew.steps=steps;
+            testcaseNew.data=data;
+            testCaseList.add(testcaseNew);
+        }
+        return testCaseList;
+
+    }
+
+    private Object getValue(HashMap<String, Object> step, String key, Object defaultValue) {
+        return step.getOrDefault(key, defaultValue);
+    }
+
+    /**
+     * 替换yaml中的一些变量，复杂结构需要用递归
+     * @param step
+     * @param key
+     * @return
+     */
+    private Object getValue(HashMap<String, Object> step, String key) {
+        Object value=step.get(key);
+        if (value instanceof String){
+            //进行替换 todo：复杂结构支持
+            return ((String) value).replace("${data}", data.get(index));
+        }else{
+            return value;
+        }
+    }
+
+
+    public void run() {
+        steps.forEach(step -> {
+            if (step.keySet().contains("chrome")) {
                 driver = new ChromeDriver();
             }
-            if(step.keySet().contains("implicitly_wait")){
-                driver.manage().timeouts().implicitlyWait(
-                        (int) step.getOrDefault("implicitly_wait", 5), TimeUnit.SECONDS);
+            if (step.keySet().contains("quit")) {
+                driver.quit();
             }
-            if(step.keySet().contains("get")){
-                driver.get(step.get("get").toString());
+            if (step.keySet().contains("implicitly_wait")) {
+                driver.manage().timeouts().implicitlyWait(
+                        (int) getValue(step, "implicitly_wait", 5), TimeUnit.SECONDS);
+            }
+            if (step.keySet().contains("get")) {
+                driver.get(getValue(step, "get").toString());
             }
 
-            if(step.keySet().contains("find")){
-                ArrayList<By> bys=new ArrayList<>();
-                ((HashMap<String, String>)step.get("find")).entrySet().forEach(stringStringEntry -> {
-                    if(stringStringEntry.getKey().contains("id")){
+            if (step.keySet().contains("find")) {
+                ArrayList<By> bys = new ArrayList<>();
+                ((HashMap<String, String>) getValue(step, "find")).entrySet().forEach(stringStringEntry -> {
+                    if (stringStringEntry.getKey().contains("id")) {
                         bys.add(By.id(stringStringEntry.getValue()));
                     }
 
-                    if(stringStringEntry.getKey().contains("xpath")){
+                    if (stringStringEntry.getKey().contains("xpath")) {
                         bys.add(By.xpath(stringStringEntry.getValue()));
                     }
 
                 });
-                currentElement =driver.findElement(bys.get(0));
+                currentElement = driver.findElement(bys.get(0));
             }
 
-            if(step.keySet().contains("click")){
+            if (step.keySet().contains("click")) {
                 currentElement.click();
             }
-            if(step.keySet().contains("send_keys")) {
+            if (step.keySet().contains("send_keys")) {
                 //todo: 参数化
-                currentElement.sendKeys("search demo");
+                currentElement.sendKeys(getValue(step, "send_keys").toString());
             }
         });
     }
